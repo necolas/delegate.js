e6868ccea42f5e15b90b21628d315df2afda25b5
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-05-07 01:55:41 -0700

    Implement `delegate()`
    If the argument is an Element or Document Node, return `delegate`.

--- delegate.js
+++ delegate.js
@@ -4,7 +4,44 @@
 
 // ---------------------------------------------------------------------------
 
+var elem;
 
+/**
+ * Specify the event delegate element
+ *
+ * @param {Element} node
+ * @return {delegate}
+ * @api public
+ */
+
+function delegate(node) {
+    if (!_isElem(node)) {
+        throw new Error('delegate(): The argument must be an Element or Document Node');
+    }
+    elem = node;
+    return delegate;
+}
+
+/**
+ * Determine is a Node is an Element or Document
+ *
+ * @param {Node} node
+ * @return {Boolean}
+ * @api private
+ */
+
+function _isElem(node) {
+    if (node && node.nodeName && (node.nodeType == Node.ELEMENT_NODE || node.nodeType == Node.DOCUMENT_NODE)) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Expose delegate
+ */
+
+return delegate;
 
 // ---------------------------------------------------------------------------
 
