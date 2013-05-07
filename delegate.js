866559c434743fbc7f6b7b2227b666b9b6da462f
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-05-07 01:58:39 -0700

    Implement `delegate.on()`

--- delegate.js
+++ delegate.js
@@ -23,6 +23,59 @@ function delegate(node) {
 }
 
 /**
+ * Delegate the handling of an event type to the given ancestor-element of
+ * nodes matching a given CSS selector. The callback function is invoked when
+ * an event bubbles up through any nodes that delegated their event handling to
+ * the ancestor.
+ *
+ * @param {String} type DOM Event type
+ * @param {String} selector
+ * @param {Function} callback
+ * @param {Boolean} [capture]
+ * @return {Function}
+ * @api public
+ */
+
+delegate.on = function (type, selector, callback, capture) {
+    function wrapper(e) {
+        // if this event has a delegateTarget, then we add it to the event
+        // object (so that handlers may have a reference to the delegator
+        // element) and fire the callback
+        if (e.delegateTarget = _getDelegateTarget(elem, e.target, selector)) {
+            callback.call(elem, e);
+        }
+    }
+
+    callback._delegateWrapper = wrapper;
+    elem.addEventListener(type, wrapper, capture || false);
+    return callback;
+};
+
+/**
+ * Walk up the DOM tree from the `target` element to which the event was
+ * dispatched, up to the delegate `elem`. If at any step, a node matches the
+ * given CSS `selector` then we know the event bubbled up through the
+ * delegator.
+ *
+ * @param {Element} elem
+ * @param {Element} target
+ * @param {String} selector
+ * @return {Element|null}
+ * @api private
+ */
+
+function _getDelegateTarget(elem, target, selector) {
+    while (target && target !== elem) {
+        if (matches(target, selector)) {
+            return target;
+        }
+        target = target.parentElement;
+    }
+
+    return null;
+}
+
+/**
  * Determine is a Node is an Element or Document
  *
  * @param {Node} node
